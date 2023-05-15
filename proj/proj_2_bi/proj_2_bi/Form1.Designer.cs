89ffab66ba85d1514126e41871bd058f986ab500
Paulo-Renato-Durante <110928059+Paulo-Renato-Durante@users.noreply.github.com>
2023-05-15 19:55:42 -0300

    projeto

---
+++ proj/proj_2_bi/proj_2_bi/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace proj_2_bi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_linha = new System.Windows.Forms.Button();
+            this.btn_circulo = new System.Windows.Forms.Button();
+            this.btn_losangulo = new System.Windows.Forms.Button();
+            this.btn_triangulo = new System.Windows.Forms.Button();
+            this.btn_pentagono = new System.Windows.Forms.Button();
+            this.btn_quadrado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_linha
+            // 
+            this.btn_linha.Location = new System.Drawing.Point(52, 12);
+            this.btn_linha.Name = "btn_linha";
+            this.btn_linha.Size = new System.Drawing.Size(30, 23);
+            this.btn_linha.TabIndex = 0;
+            this.btn_linha.Text = "/";
+            this.btn_linha.UseVisualStyleBackColor = true;
+            this.btn_linha.Click += new System.EventHandler(this.btn_linha_Click);
+            // 
+            // btn_circulo
+            // 
+            this.btn_circulo.Location = new System.Drawing.Point(88, 13);
+            this.btn_circulo.Name = "btn_circulo";
+            this.btn_circulo.Size = new System.Drawing.Size(30, 23);
+            this.btn_circulo.TabIndex = 1;
+            this.btn_circulo.Text = "O";
+            this.btn_circulo.UseVisualStyleBackColor = true;
+            this.btn_circulo.Click += new System.EventHandler(this.btn_circulo_Click);
+            // 
+            // btn_losangulo
+            // 
+            this.btn_losangulo.Location = new System.Drawing.Point(160, 13);
+            this.btn_losangulo.Name = "btn_losangulo";
+            this.btn_losangulo.Size = new System.Drawing.Size(30, 23);
+            this.btn_losangulo.TabIndex = 3;
+            this.btn_losangulo.Text = "◇";
+            this.btn_losangulo.UseVisualStyleBackColor = true;
+            this.btn_losangulo.Click += new System.EventHandler(this.btn_losangulo_Click);
+            // 
+            // btn_triangulo
+            // 
+            this.btn_triangulo.Location = new System.Drawing.Point(196, 13);
+            this.btn_triangulo.Name = "btn_triangulo";
+            this.btn_triangulo.Size = new System.Drawing.Size(30, 23);
+            this.btn_triangulo.TabIndex = 4;
+            this.btn_triangulo.Text = "△";
+            this.btn_triangulo.UseVisualStyleBackColor = true;
+            this.btn_triangulo.Click += new System.EventHandler(this.btn_triangulo_Click);
+            // 
+            // btn_pentagono
+            // 
+            this.btn_pentagono.Location = new System.Drawing.Point(232, 13);
+            this.btn_pentagono.Name = "btn_pentagono";
+            this.btn_pentagono.Size = new System.Drawing.Size(30, 23);
+            this.btn_pentagono.TabIndex = 5;
+            this.btn_pentagono.Text = "⭔";
+            this.btn_pentagono.UseVisualStyleBackColor = true;
+            this.btn_pentagono.Click += new System.EventHandler(this.btn_pentagono_Click);
+            // 
+            // btn_quadrado
+            // 
+            this.btn_quadrado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btn_quadrado.Location = new System.Drawing.Point(124, 13);
+            this.btn_quadrado.Name = "btn_quadrado";
+            this.btn_quadrado.Size = new System.Drawing.Size(30, 23);
+            this.btn_quadrado.TabIndex = 2;
+            this.btn_quadrado.Text = "⬜";
+            this.btn_quadrado.UseVisualStyleBackColor = true;
+            this.btn_quadrado.Click += new System.EventHandler(this.btn_quadrado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_pentagono);
+            this.Controls.Add(this.btn_triangulo);
+            this.Controls.Add(this.btn_losangulo);
+            this.Controls.Add(this.btn_quadrado);
+            this.Controls.Add(this.btn_circulo);
+            this.Controls.Add(this.btn_linha);
+            this.Name = "Form1";
+            this.Text = "paint";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_linha;
+        private System.Windows.Forms.Button btn_circulo;
+        private System.Windows.Forms.Button btn_losangulo;
+        private System.Windows.Forms.Button btn_triangulo;
+        private System.Windows.Forms.Button btn_pentagono;
+        private System.Windows.Forms.Button btn_quadrado;
+    }
+}
+
---
+++ proj/proj_2_bi/proj_2_bi/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proj_2_bi
+{
+    public partial class Form1 : Form
+    {
+        int click;
+        int[] pontos = new int[10];
+        bool linha, circulo, quadrado, losango, triangulo, pentagono;
+        public Color cor(int r, int g, int b)
+        {
+            return Color.FromArgb(r, g, b);
+        }
+        public Pen caneta(Color cor, int espessura)
+        {
+            return new Pen(cor, espessura);
+        }
+        public void retaBreseham(int x0, int y0, int x1, int y1, Pen caneta, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(caneta, x0, y0, x1, y1);
+        }
+        public void pintap(int x, int y,Pen caneta, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(caneta, x, y, x + 1, y);
+        }
+        public void desenhaQuadrilatero(PaintEventArgs e,int x, int y, int altura, int largura, Pen caneta)
+        {
+            e.Graphics.DrawRectangle(caneta, x, y, largura, altura);
+        }
+        public void desenharCirculo(int xc, int yc, int raio,Pen caneta, PaintEventArgs e)
+        {
+            e.Graphics.DrawEllipse(caneta, xc, yc, raio, raio);
+        }
+        public void desenharElipse(int xc, int yc, int raiox, int raioy, Pen caneta, PaintEventArgs e)
+        {
+            e.Graphics.DrawEllipse(caneta, xc, yc, raiox, raioy);
+        }
+
+        public void desenhaFormas(PaintEventArgs e, int[] pontos,Pen caneta)
+        {
+            for (int i = 0; i < pontos.Length - 2; i += 2)
+            {
+                retaBreseham(pontos[i], pontos[i + 1], pontos[i + 2], pontos[i + 3], caneta, e);
+            }
+            retaBreseham(pontos[0], pontos[1], pontos[pontos.Length - 2], pontos[pontos.Length - 1], caneta, e);
+        }
+        public void setFalse()
+        {
+            linha = false;
+            circulo = false;
+            quadrado = false;
+            losango = false;
+            triangulo = false;
+            pentagono = false;
+        }
+        public void zerarPontos()
+        {
+            for(int i = 0; i <= pontos.Length - 1; i++)
+            {
+                pontos[i] = 0;
+            }
+            click = 0;
+        }
+        
+
+
+        private void btn_linha_Click(object sender, EventArgs e)
+        {
+            zerarPontos();
+            setFalse();
+            linha = true;
+        }
+
+        private void btn_circulo_Click(object sender, EventArgs e)
+        {
+            zerarPontos();
+            setFalse();
+            circulo = true;
+        }
+
+        private void btn_quadrado_Click(object sender, EventArgs e)
+        {
+            zerarPontos();
+            setFalse();
+            quadrado = true;
+        }
+
+        private void btn_losangulo_Click(object sender, EventArgs e)
+        {
+            zerarPontos();
+            setFalse();
+            losango = true;
+        }
+
+        private void btn_triangulo_Click(object sender, EventArgs e)
+        {
+            zerarPontos();
+            setFalse();
+            triangulo = false;
+        }
+
+        private void btn_pentagono_Click(object sender, EventArgs e)
+        {
+            zerarPontos();
+            setFalse();
+            pentagono = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            click++;
+            
+            pontos[(click*2)-2] = e.X;
+            pontos[(click*2)-1] = e.Y;
+            if (linha == true && click == 2)
+            {
+                Invalidate();
+            }
+            else if(triangulo == true && click == 3)
+            {
+                Invalidate();
+            }
+            else if(quadrado == true && click == 4)
+            {
+                Invalidate();
+            }
+            else if(losango == true && click == 4)
+            {
+                Invalidate();
+            }
+        }
+
+        private void Form1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            this.Text = e.X.ToString() + " " + e.Y.ToString();
+        }
+        public void controladorLinha(PaintEventArgs e)
+        {
+            if (click % 2 == 0)
+            {
+                retaBreseham(pontos[0], pontos[1], pontos[2], pontos[3], caneta(cor(255, 0, 0), 2), e);
+            }
+        }
+        public void controladorTriangulo(PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            if (linha)
+            {
+                controladorLinha(e);
+                zerarPontos();
+            }
+            else if (circulo)
+            {
+
+            }else if (quadrado)
+            {
+
+            }else if (losango)
+            {
+
+            }else if (triangulo)
+            {
+                triangulo
+
+            }else if (pentagono)
+            {
+
+            }
+        
+            
+            
+           
+
+        }
+    }
+}
